8ed7154f598788a75dbf4000cb10bbcfb30b1e28
SepehrJ <Sepehristaken@gmail.com>
2024-06-21 09:40:24 +0330

    Refactoring Main.java in question02

--- untitled/src/main/java/question02/Main.java
+++ untitled/src/main/java/question02/Main.java
@@ -3,6 +3,7 @@ package question02;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.TreeSet;
+import java.util.NoSuchElementException;
 
 public class Main {
     private static TreeSet<Character> first = new TreeSet<>();
@@ -10,39 +11,54 @@ public class Main {
     private static Random random = new Random();
     private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    public void fill(TreeSet<Character> treeSet){
-        for (int i = 0; i < 10; i++) {
-            treeSet.add(alphabet.charAt(random.nextInt(alphabet.length())));
+    public void fill(TreeSet<Character> treeSet) {
+        try {
+            for (int i = 0; i < 10; i++) {
+                treeSet.add(alphabet.charAt(random.nextInt(alphabet.length())));
+            }
+        } catch (Exception e) {
+            System.out.println("An error occurred while filling the TreeSet: " + e.getMessage());
         }
     }
 
-    public TreeSet<Character> combinedSet(TreeSet<Character> first, TreeSet<Character> second){
-        TreeSet<Character> combinedSet = new TreeSet<>(first);
-        combinedSet.addAll(second);
-        return combinedSet;
+    public TreeSet<Character> combinedSet(TreeSet<Character> first, TreeSet<Character> second) {
+        try {
+            TreeSet<Character> combinedSet = new TreeSet<>(first);
+            combinedSet.addAll(second);
+            return combinedSet;
+        } catch (Exception e) {
+            System.out.println("An error occurred while combining sets: " + e.getMessage());
+            return new TreeSet<>();
+        }
     }
 
-    public TreeSet<Character> commonSet(TreeSet<Character> first, TreeSet<Character> second){
+    public TreeSet<Character> commonSet(TreeSet<Character> first, TreeSet<Character> second) {
         TreeSet<Character> commonSet = new TreeSet<>();
-        Iterator<Character> firstIterator = first.iterator();
-        Iterator<Character> secondIterator = second.iterator();
-        while(firstIterator.hasNext()){
-            if(firstIterator.next().equals(secondIterator.next())){
-                commonSet.add(firstIterator.next());
+        try {
+            Iterator<Character> firstIterator = first.iterator();
+            while (firstIterator.hasNext()) {
+                Character element = firstIterator.next();
+                if (second.contains(element)) {
+                    commonSet.add(element);
+                }
             }
+        } catch (Exception e) {
+            System.out.println("An error occurred while finding common elements: " + e.getMessage());
         }
         return commonSet;
     }
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.fill(first);
-        main.fill(second);
-        System.out.println("characters of first TreeSet:" + first);
-        System.out.println("characters of second TreeSet:" + second);
-        System.out.println(main.combinedSet(first, second));
-        System.out.println(main.commonSet(first, second));
+        try {
+            Main main = new Main();
+            main.fill(first);
+            main.fill(second);
+            System.out.println("Characters of first TreeSet: " + first);
+            System.out.println("Characters of second TreeSet: " + second);
+            System.out.println("Combined Set: " + main.combinedSet(first, second));
+            System.out.println("Common Set: " + main.commonSet(first, second));
+        } catch (Exception e) {
+            System.out.println("An error occurred in the main method: " + e.getMessage());
+        }
     }
-
-
 }
