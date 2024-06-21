1d3d9b5a714421590ab1dc08aed5d4281e4f7c62
SepehrJ <Sepehristaken@gmail.com>
2024-06-21 09:38:42 +0330

    Refactoring Main.java in question03

--- untitled/src/main/java/question03/Main.java
+++ untitled/src/main/java/question03/Main.java
@@ -1,29 +1,54 @@
 package question03;
 
 import java.util.ArrayList;
-
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
-   public static void removeUnsuitablePairs(ArrayList<Integer> list){
-      for(int i = 0; i < list.size() - 1; i += 2){
-         if(list.get(i) > list.get(i + 1)){
-            list.remove(i);
-            list.remove(i);
-            i -= 2;
-         }
-      }
-      if(list.size() % 2 != 0){
-         list.remove(list.size() -1);
-      }
-   }
-   public static void main(String[] args) {
-      ArrayList<Integer> list = new ArrayList<>();
-      list.add(1); list.add(3); list.add(7); list.add(4); list.add(3); list.add(6);
-      list.add(5); list.add(8); list.add(5); list.add(5); list.add(2);
-      list.add(7); list.add(3);
+    public static void removeUnsuitablePairs(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i += 2) {
+            if (list.get(i) > list.get(i + 1)) {
+                list.remove(i);
+                list.remove(i);
+                i -= 2;
+                if (i < -1) {
+                    i = -1;
+                }
+            }
+        }
+        if (list.size() % 2 != 0) {
+            list.remove(list.size() - 1);
+        }
+    }
 
-      System.out.println("Original List: " + list);
-      removeUnsuitablePairs(list);
-      System.out.println("Modified List: " + list);
-   }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        try {
+            System.out.println("Enter how many numbers you want to enter: ");
+            int size = input.nextInt();
+
+            if (size <= 0) {
+                throw new IllegalArgumentException("Size must be a positive integer.");
+            }
+
+            for (int i = 0; i < size; i++) {
+                System.out.println("Number " + (i + 1) + " : ");
+                int num = input.nextInt();
+                list.add(num);
+            }
+
+            System.out.println("Original List: " + list);
+            removeUnsuitablePairs(list);
+            System.out.println("Modified List: " + list);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter integers only.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            input.close();
+        }
+    }
 }
